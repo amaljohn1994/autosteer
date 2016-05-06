b0f8216de9ca049937b3aadb357978d1b9fccd32
Hari <harikrishnanr195@gmail.com>
2016-05-07 03:04:29 +0530

    function to convert feature to text is in progress

--- brain.py
+++ brain.py
@@ -40,7 +40,7 @@ def cleanVideo(labelPath,featurePath):
         ret, frame = cam.read()
         if ret==True:
             gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-            cv2.imshow('frame',gray)
+            #cv2.imshow('frame',gray)
             if (labels[i]<sensorLow)|(labels[i]>sensorHigh):
                 continue
             temp=np.vstack((cleanLabels,labels[i]))
@@ -65,9 +65,46 @@ def setNetParams():
         netParams[0,i]=int(raw_input("Enter no. of neurons in layer %d:"%c))
     return netParams[0]
 
+def setTrainParams():
+    moment=float(raw_input("Enter moment scale:"))
+    maxIter=int(raw_input("Enter maximum iterations:"))
+    maxError=float(raw_input("Enter eror change to stop:"))
+    return maxIter,maxError,moment
 
-#sensorLow,sensorHigh=setSensorLimits()
-#labelPath=chooseLabelPath()
-#featurePath=chooseFeaturePath()
-#total,cleanLabels,cleanVideoPath=cleanVideo(labelPath,featurePath)
-netParams=etNetParams()
+def convertFeature(cleanVideoPath):
+    print cleanVideoPath
+    cam = cv2.VideoCapture(cleanVideoPath)
+    final=np.zeros((1,0),'float')
+    a=np.zeros((1,0),'float')
+    while(cam.isOpened()):
+        ret,frame=cam.read()
+        cv2.imshow('frame',frame)
+        if ret==True:
+            temp=np.reshape(frame,(1,len(frame)*len(frame[0])))
+            print temp.shape
+            a=np.vstack(final,temp)
+            final=a
+    print final.shape
+    cam.release()
+    final=final+0.0
+    return final
+
+def trainNet(netParams,maxIter,maxError,moment,cleanLabels,cleanFeatures):
+    model=cv2.ANN_MLP()
+    model.create(netParams)
+    criteria = (cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, maxIter, maxError)
+    params = dict(term_crit = criteria,
+                   train_method = cv2.ANN_MLP_TRAIN_PARAMS_BACKPROP,
+                   bp_dw_scale = moment,
+                   bp_moment_scale = 0.0 )
+    num_iter=model.train(cleanFeatures,cleanLabels,None,params=params)
+    print num_iter
+
+sensorLow,sensorHigh=setSensorLimits()
+labelPath=chooseLabelPath()
+featurePath=chooseFeaturePath()
+total,cleanLabels,cleanVideoPath=cleanVideo(labelPath,featurePath)
+netParams=setNetParams()
+maxIter,maxError,moment=setTrainParams()
+cleanFeatures=convertFeature(cleanVideoPath)
+trainNet(netParams,maxIter,maxError,moment,cleanLabels,cleanFeatures)
