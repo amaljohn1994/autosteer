aac2662735501280eb13c9f739a703d0bf930fe9
Hari <harikrishnanr195@gmail.com>
2016-05-07 12:24:27 +0530

    Splited to precessing.py

--- brain.py
+++ brain.py
@@ -5,28 +5,6 @@ import cv2
 import cv2.cv as cv
 import time
 
-def setSensorLimits():
-    l=raw_input("Enter lower threshold value of sensor")
-    h=raw_input("Enter higher threshold value of sensor")
-    return float(l),float(h)
-
-def chooseFeaturePath():
-    print "Select path for features"
-    root=tk.Tk()
-    root.withdraw()
-    a=filedialog.askopenfilename()
-    return a
-
-
-def chooseLabelPath():
-    print "Select path for labels"
-    root=tk.Tk()
-    root.withdraw()
-    a=filedialog.askopenfilename()
-    return a
-
-
-
 def setNetParams():
     t=int(raw_input("Total number of layers for network:"))
     netParams=np.zeros((1,t),'int32')
@@ -41,24 +19,6 @@ def setTrainParams():
     maxError=float(raw_input("Enter eror change to stop:"))
     return maxIter,maxError,moment
 
-def convertFeature(cleanVideoPath):
-    print cleanVideoPath
-    cam = cv2.VideoCapture(cleanVideoPath)
-    final=np.zeros((1,0),'float')
-    a=np.zeros((1,0),'float')
-    while(cam.isOpened()):
-        ret,frame=cam.read()
-        cv2.imshow('frame',frame)
-        if ret==True:
-            temp=np.reshape(frame,(1,len(frame)*len(frame[0])))
-            print temp.shape
-            a=np.vstack(final,temp)
-            final=a
-    print final.shape
-    cam.release()
-    final=final+0.0
-    return final
-
 def trainNet(netParams,maxIter,maxError,moment,cleanLabels,cleanFeatures):
     model=cv2.ANN_MLP()
     model.create(netParams)
@@ -70,11 +30,6 @@ def trainNet(netParams,maxIter,maxError,moment,cleanLabels,cleanFeatures):
     num_iter=model.train(cleanFeatures,cleanLabels,None,params=params)
     print num_iter
 
-sensorLow,sensorHigh=setSensorLimits()
-labelPath=chooseLabelPath()
-featurePath=chooseFeaturePath()
-total,cleanLabels,cleanVideoPath=cleanVideo(labelPath,featurePath)
 netParams=setNetParams()
 maxIter,maxError,moment=setTrainParams()
-cleanFeatures=convertFeature(cleanVideoPath)
 trainNet(netParams,maxIter,maxError,moment,cleanLabels,cleanFeatures)
--- processing.py
+++ processing.py
@@ -1,3 +1,48 @@
+import Tkinter as tk
+import tkFileDialog as filedialog
+import numpy as np
+import cv2
+import cv2.cv as cv
+import time
+
+def setSensorLimits():
+    l=raw_input("Enter lower threshold value of sensor")
+    h=raw_input("Enter higher threshold value of sensor")
+    return float(l),float(h)
+
+def chooseFeaturePath():
+    print "Select path for features"
+    root=tk.Tk()
+    root.withdraw()
+    a=filedialog.askopenfilename()
+    return a
+
+
+def chooseLabelPath():
+    print "Select path for labels"
+    root=tk.Tk()
+    root.withdraw()
+    a=filedialog.askopenfilename()
+    return a
+
+def convertFeature(cleanVideoPath):
+    print cleanVideoPath
+    cam = cv2.VideoCapture(cleanVideoPath)
+    final=np.zeros((1,0),'float')
+    a=np.zeros((1,0),'float')
+    while(cam.isOpened()):
+        ret,frame=cam.read()
+        cv2.imshow('frame',frame)
+        if ret==True:
+            temp=np.reshape(frame,(1,len(frame)*len(frame[0])))
+            print temp.shape
+            a=np.vstack(final,temp)
+            final=a
+    print final.shape
+    cam.release()
+    final=final+0.0
+    return final
+
 def cleanVideo(labelPath,featurePath):
     labels=np.zeros((0,1),'float')
     cleanLabels=np.zeros((0,1),'float')
@@ -28,3 +73,9 @@ def cleanVideo(labelPath,featurePath):
     out.release()
     cv2.destroyAllWindows()
     return count,cleanLabels,"cleanVideo.avi"
+
+sensorLow,sensorHigh=setSensorLimits()
+labelPath=chooseLabelPath()
+featurePath=chooseFeaturePath()
+total,cleanLabels,cleanVideoPath=cleanVideo(labelPath,featurePath)
+cleanFeatures=convertFeature(cleanVideoPath)
